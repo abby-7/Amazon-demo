b99bf4ebac70c1b6a626331522f30e052206d827
yuehanl2 <55108239+yuehanl2@users.noreply.github.com>
2023-01-25 14:37:47 -0500

    Add Box1

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
+import './homepage.css';
 
 import Header from '../Header/header';
 import Navbar from '../Navbar/navbar';
+import ItemBox1 from './HomepageComponment/itemBox';
 
 function HomePage(){
 
     return(<>
       <Header />
       <Navbar />
+      <ItemBox1 />
    
     </>)
 }
